8370e89a26514f74922cdd2329007b0c7f3078e1
KidusHaileleul <haileleul.kidus@gmail.com>
2025-07-31 15:27:46 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (96.02%) - LeetSync

---
+++ 777-toeplitz-matrix/toeplitz-matrix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        row = len(matrix)
+        cols = len(matrix[0])
+        for i in range(row-1):
+            for j in range(cols -1):
+                if matrix[i][j]!= matrix[i+1][j+1]:
+                    return False
+        return True
+        
