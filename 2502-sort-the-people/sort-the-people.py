ff7df2632a474cac39f0ca74dbf32cb23af4610d
KidusHaileleul <haileleul.kidus@gmail.com>
2025-08-12 20:37:31 +0300

    Time: 479 ms (5.73%) | Memory: 18.2 MB (48.86%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,6 +1,9 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         paired = list(zip(heights, names))
-        new_paired=list(reversed(sorted(paired)))
-        return [name for height, name in new_paired]
+        for i in range(len(paired)):
+            for j in range(i+1, len(paired)):
+                if paired[i] < paired[j]:  
+                    paired[i], paired[j] = paired[j], paired[i]
+        return [name for _, name in paired]
         
