30293f702d2f2dc845c28e1c6d27f6efe95f5207
KidusHaileleul <haileleul.kidus@gmail.com>
2025-08-19 23:04:53 +0300

    Time: 834 ms (20.97%) | Memory: 20.8 MB (35.98%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,24 +4,25 @@ class Solution:
         x = []
         n = len(nums)
         for i in range(n-2):
-            if i > 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            l, r = i+1, n-1
+            l = i + 1
+            r = n - 1
             while l < r:
-                s = nums[i] + nums[l] + nums[r]
-                if s == 0:
+                if nums[i] + nums[l] + nums[r] == 0:
                     x.append([nums[i], nums[l], nums[r]])
                     l += 1
                     r -= 1
-                    while l < r and nums[l] == nums[l-1]:
+                    while l < r and nums[l] == nums[l - 1]:
                         l += 1
-                    while l < r and nums[r] == nums[r+1]:
+                    while l < r and nums[r] == nums[r + 1]:
                         r -= 1
-                elif s < 0:
-                    l += 1
-                else:
+                elif nums[i] + nums[l] + nums[r] > 0:
                     r -= 1
+                else:
+                    l += 1
         return x
+
 
                 
 
